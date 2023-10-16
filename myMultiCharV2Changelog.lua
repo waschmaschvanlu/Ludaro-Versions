e19dee8e0f9fbb87f3cd463dc8cbd4d8c4911c6c
Ludaro <waschmaschvanlu@gmail.com>
2023-10-16 11:42:18 +0200

    Update myMultiCharV2Changelog.lua

--- myMultiCharV2Changelog.lua
+++ myMultiCharV2Changelog.lua
@@ -1,3 +1,3 @@
-"beta01" = "Added the Version-Checker"
-"beta02" = "Fixed a bug with the spawnselection bc its not done yet"
-"beta06" = "lol"
+beta01 = "Added the Version-Checker"
+beta02 = "Fixed a bug with the spawnselection bc its not done yet"
+beta06 = "lol"
