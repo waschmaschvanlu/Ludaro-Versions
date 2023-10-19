ad0dc606d8e38ba5d1049891f58c6c3307b4767c
Ludaro <waschmaschvanlu@gmail.com>
2023-10-19 13:55:17 +0200

    Update myMultiCharV2Changelog.lua

--- myMultiCharV2Changelog.lua
+++ myMultiCharV2Changelog.lua
@@ -1,3 +1,4 @@
 beta01 = "Added the Version-Checker"
 beta02 = "Fixed a bug with the spawnselection bc its not done yet"
 beta07 = "Fixed jaksam vehicleKeys not Resetting keys upon relog, and fixed a bug where relog would respawn you in the wrong spot, and fixed a bug that would set your player under the map"
+beta08 = "Fixed a bug where you could be stuck in mainmenu and had to relog and fixed that errors come up when certain chars had missing stuff"
